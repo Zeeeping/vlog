bf1b14524013b5e0bca63e5e8703315f75502f30
Zeeep <zengping@ygsoft.com>
2022-03-29 17:50:52 +0800

    feat(#):20220329[change app.js]更新 static 中间件的使用 demo

--- node-express/src/app.js
+++ node-express/src/app.js
@@ -13,8 +13,11 @@ app.get('/user', (req, res) => {
   res.json({ code: 0, message: 'get user' })
 })
 
-// 必须加斜杠
-app.use('/static', express.static('public'))
+// 必须加斜杠，映射到静态文件 public 目录
+// 使用一下写法需注意：如果不使用命令行启动，需要从根目录使用 node src/app 启动项目
+// app.use('/static', express.static('public'))
+// 也可以不带路径，但都需要从根目录启动项目
+app.use(express.static('public'))
 
 app.listen('8001', () => {
   console.log('app running at port 8001')

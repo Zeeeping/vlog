fbb78c5ce9864654a61556982fdd76a69f369509
Zeeep <zengping@ygsoft.com>
2022-03-29 20:57:42 +0800

    feat(#):20220329[add files]添加测试用例 demo 代码

---
+++ typescript-jest/src/demo/__tests__/demo.test.ts
@@ -0,0 +1,6 @@
+// demo.test.ts
+import { sum } from '../index'
+
+it('这是测试用例输出的内容', () => {
+  expect(sum(1, 1)).toBe(2)
+})
---
+++ typescript-jest/src/demo/index.ts
@@ -0,0 +1,4 @@
+// index.ts
+export function sum(a, b) {
+  return a + b
+}

661ad6ead761dabf3b0abd6d102e3b679ee75510
Zeeep <zengping@ygsoft.com>
2022-03-30 14:50:59 +0800

    feat(#):20220330[update demo.test]更新 demo 测试用例代码，添加注释

--- typescript-jest/src/demo/__tests__/demo.test.ts
+++ typescript-jest/src/demo/__tests__/demo.test.ts
@@ -1,6 +1,11 @@
 // demo.test.ts
 import { sum } from '../index'
 
-it('这是测试用例输出的内容', () => {
-  expect(sum(1, 1)).toBe(2)
+// jest 命令使用正则匹配文件名或文件目录
+// 需要 __tests__ 目录下或者带 spec|test 的文件名
+// 使用 yarn test demo(__tests__下测试用例的文件名，本例中就是 demo)
+describe('这是测试用例输出的描述', () => {
+  it('这是测试用例输出的内容', () => {
+    expect(sum(1, 1)).toBe(2)
+  })
 })
